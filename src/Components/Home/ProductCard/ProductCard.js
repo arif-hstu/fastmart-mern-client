41ae31bf3818242d28d940e1c21a1605d3979640
arif-hstu <arif.dvm.hstu@gmail.com>
2021-05-22 19:38:52 +0600

    animated cart added

--- src/Components/Home/ProductCard/ProductCard.js
+++ src/Components/Home/ProductCard/ProductCard.js
@@ -1,17 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { CartContext, UserContext } from '../../../App';
 import './ProductCard.css'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
-
 function ProductCard({ pd }) {
 	const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+	const previousStyle = {
+		opacity: '1',
+		transition: '0.1s'
+	}
+	const alteredStyle = {
+		transform: 'translateY(-50px)',
+		opacity: '0',
+		transition: '0.1s'
+
+	}
+	const [animatedStyle, setAnimatedStyle] = useState(previousStyle);
 
 	// handle cart
 	const [cart, setCart] = useContext(CartContext);
 	const addToCart = (e) => {
 		e.preventDefault();
+
 		if (e.target.parentNode.id || e.target.id) {
+			setAnimatedStyle(alteredStyle);
 			const newCart = [...cart, e.target.parentNode.id || e.target.id];
 			setCart(newCart);
 
@@ -20,19 +33,22 @@ function ProductCard({ pd }) {
 			newUserInfo.cart = newCart;
 			setLoggedInUser(newUserInfo);
 		}
+
+		setTimeout(function() { setAnimatedStyle(previousStyle) }, 300);
+
 	}
 
 	return (
 		<div onClick={addToCart} id={pd._id} className='ProductCard'>
-			<div className="imageHolder">
-				<img src={pd.pdImage} alt="Product Image" />
+			<div id={pd._id} className="imageHolder">
+				<img id={pd._id} src={pd.pdImage} alt="Product Image" />
 			</div>
 			<p id={pd._id}><small>{pd.pdCatagory}</small></p>
 			<h5>{pd.pdName}</h5>
 			<div className='priceCart' id={pd._id}>
 				<h5 className='highlighted'>${pd.pdPrice}</h5>
 				<div className='cart' id={pd._id}>
-					<ShoppingCartIcon id={pd._id} style={{ color: '#fa4f90' }} />
+					<ShoppingCartIcon style={animatedStyle} id={pd._id} />
 					<p className='highlighted'>Add to</p>
 					<p className='highlighted'>Cart</p>
 				</div>
