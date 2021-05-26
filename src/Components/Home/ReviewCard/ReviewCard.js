89b80559e2298970b0da1a19d4f4db3a7cda8892
arif-hstu <arif.dvm.hstu@gmail.com>
2021-05-26 16:42:27 +0600

    category icon color changed

--- src/Components/Home/ReviewCard/ReviewCard.js
+++ src/Components/Home/ReviewCard/ReviewCard.js
@@ -5,7 +5,9 @@ function ReviewCard({ reivewData }) {
 	return (
 		<div className='ReviewCard'>
 			<img src={reivewData.img} alt="Avatar" />
-			<p>{reivewData.review}</p>
+			<div className="reviewHolder">
+				<p>{reivewData.review}</p>
+			</div>
 			<h4>{reivewData.name}</h4>
 			<h6>{reivewData.designation}</h6>
 		</div>
